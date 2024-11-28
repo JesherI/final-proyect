70fb37973e05ed14be25f4402b9ba099db26e01d
Jehsersin_09 <jeshermendieta@gmail.com>
2024-11-28 07:47:31 -0500

    fist version

---
+++ products/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
