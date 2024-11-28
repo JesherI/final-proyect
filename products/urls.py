b43e6199413f70ac441f349fa5d91786dc3c8cb5
Jehsersin_09 <jeshermendieta@gmail.com>
2024-11-28 09:45:01 -0500

    CRUD completo de marca

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('marcas/', views.lista_marcas, name='lista-marcas'),
     path('crear-marcas/', views.crear_marca, name='crear-marca'),
+    path('eliminar-marca/<int:pk>/', views.eliminar_marca, name='eliminar-marca'),  # URL para eliminar
+    path('actualizar-marca/<int:pk>/', views.actualizar_marca, name='actualizar-marca'),  # URL para actualizar
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import MarcaForm
 from .models import Marca
 # Create your views here.
 
@@ -14,3 +15,21 @@ def crear_marca(request):
 def lista_marcas(request):
     marcas = Marca.objects.all()
     return render(request, 'products/listamarca.html', {'marcas': marcas})
+
+def eliminar_marca(request, pk):
+    marca = get_object_or_404(Marca, pk=pk)  # Obtiene la marca por el id (pk)
+    if request.method == 'POST':
+        marca.delete()  # Elimina la marca
+        return redirect('lista-marcas')  # Redirige a la lista de marcas
+    return render(request, 'products/confirmar_eliminacion.html', {'marca': marca})
+
+def actualizar_marca(request, pk):
+    marca = get_object_or_404(Marca, pk=pk)  # Obtiene la marca por id
+    if request.method == 'POST':
+        form = MarcaForm(request.POST, instance=marca)  # Pasa el formulario con los datos actuales
+        if form.is_valid():
+            form.save()  # Guarda los cambios
+            return redirect('lista-marcas')  # Redirige a la lista de marcas
+    else:
+        form = MarcaForm(instance=marca)  # Si es un GET, muestra el formulario con los datos actuales
+    return render(request, 'products/actualizar_marca.html', {'form': form}) 
